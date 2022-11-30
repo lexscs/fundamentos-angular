feb79792632e52d46c67a27534f117deab4ae30a
AlxWatanabe <lex_scs@yahoo.com.br>
2022-11-30 10:24:05 -0300

    Atualizando o primeiro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponente } from './primeiro-componente/primeiro-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponente
   ],
   imports: [
     BrowserModule,
