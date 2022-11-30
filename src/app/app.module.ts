d2df0545fd153a21886dd4754684757e2ced7a78
AlxWatanabe <lex_scs@yahoo.com.br>
2022-11-30 10:55:17 -0300

    segundo componente, componente multiplicador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponente } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponenteComponente
+    PrimeiroComponenteComponente,
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCustomizadoComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
