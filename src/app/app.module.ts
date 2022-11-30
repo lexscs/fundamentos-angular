47d077434d33ebd6b43a23b93c9387e2146bf0c2
AlxWatanabe <lex_scs@yahoo.com.br>
2022-11-30 11:24:25 -0300

    criação do component two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { PrimeiroComponenteComponente } from './primeiro-componente/primeiro-com
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 import { MultiplicaPorPipe } from './multiplica-por.pipe';
 import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -17,12 +19,14 @@ import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.co
     SegundoComponenteComponent,
     MultiplicaPorPipe,
     PipeCustomizadoComponent,
+    TwoWayDataBindingComponent,
     
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
